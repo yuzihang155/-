2736fa5ba8d43ab6fc553f729739027e6e3be8da
yuzihang <1205181793@qq.com>
2021-03-21 16:03:48 +0800

    小兔仙儿项目

--- xtx-pc-122/src/utils/request.js
+++ xtx-pc-122/src/utils/request.js
@@ -25,15 +25,39 @@ instance.interceptors.request.use(config => {
 },
 e => Promise.reject(e))
 
-// 响应拦截器
+// 3,响应拦截器
 instance.interceptors.response.user(res => res.data, e => {
   // 当token未传，已失效，响应状态码401
   if (e.response && e.response.status === 401) {
     // 当你失败的时候，获取当前的路由地址（完整地址 hash地址 + ?传参）
     // router ===>$router  路径$router.path ===> router.currentRoute.fullPath
-    const redirectUrl = router.currentRoute.fullPath
+
+    // 1,跳转的时候， /login?redirectUrl=/member/order?id=100&name-tom
+    // 2,需要对代码进行URL编码，encodeURICompenent('/member/order?id=100&name-tom')
+    const redirectUrl = encodeURIComponent(router.currentRoute.value.fullPath)
     // 跳转登录页面，并且传递当前的路由地址（目的是将来登录完毕后可以回跳）
     router.push({ path: '/login', query: { redirectUrl } })
   }
   return Promise.project(e)
 })
+
+// 4，定义一个函数使用配置好的axios发请求
+/**
+ * @param {String} -url 请求地址
+ * @param {String} -method 请求类型
+ * @param {Object} -submitData 对象类型，提交数据
+ */
+export default (url, method, submitData) => {
+  // 使用配置好的额axios来发送请求
+//   返回axios的调用，返回的就是一个promise
+  return Promise({
+    url,
+    method,
+    // params get方式的传参，  data其他方式 ，请求体
+    // params: submitData,
+    // data: submitData
+    [method.toLowerCase() === 'get' ? 'params' : 'data']: submitData
+  })
+}
+
+export default request
